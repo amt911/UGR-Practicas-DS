bd82abff77642dd0fea2d6c8c99900b8d02cd773
Andrés Merlo Trujillo <a.merlo.truji10@gmail.com>
2022-03-05 10:32:30 +0100

    A medias

--- S1M2/src/Objetivo.java
+++ S1M2/src/Objetivo.java
@@ -1,28 +1,39 @@
-public class Objetivo {
+import javax.swing.JPanel;
+
+public class Objetivo extends JPanel{
 	public EstadoMotor _tiene;
-	private double velocidad;
-	private double distancia;
-	public double revs;
+	//private double velocidad;
+	private Velocimetro velocidad;
+	//private double distancia;
+	private CuentaKilometros distancia;
+	//public double revs;
+	public 
 	final private double radio=0.15;
-	final public int MAX_REVS=5000;
+	//final public int MAX_REVS=5000;
 
 	public Objetivo(){
 		_tiene=EstadoMotor.APAGADO;
-		velocidad=0;
-		distancia=0;
+		//velocidad=0;
+		velocidad=new Velocimetro();
+		//distancia=0;
+		distancia=new CuentaKilometros();
+
 		revs=0;
 	}
 	public double ejecutar(double aRevoluciones, EstadoMotor aEstadoMotor) {
 		//throw new UnsupportedOperationException();
 		revs=aRevoluciones;
 		_tiene=aEstadoMotor;
-		distancia+=2*3.14*radio*revs;
-		velocidad=2*3.14*radio*revs*0.06;
+		//distancia+=2*3.14*radio*revs;
+		distancia.addDistancia(2*3.14*radio*revs);	//MEJORABLE
+
+		//velocidad=2*3.14*radio*revs*0.06;
+		velocidad.calcularVelocidad(radio, revs);
 
 
 		System.out.println("Revs: "+revs);
-		System.out.println("\nDistancia recorrida: "+distancia);
-		System.out.println("\nVelocidad actual: "+velocidad);
+		System.out.println("\nDistancia recorrida: "+distancia.getDistancia());
+		System.out.println("\nVelocidad actual: "+velocidad.getVelocidad());
 
 		return  5.6;	//ARREGLAR ESTE RETURN, NO SE EXACTAMENTE QUE TIENE QUE DEVOLVER
 	}
