ee7138981da4427a9d74f913a37486b0dd5dfd01
Javier Serrano Lucas <javier.serluc@gmail.com>
2022-03-06 11:41:31 +0100

    implementación método update

--- S1M1/src/GraficaTemperatura.java
+++ S1M1/src/GraficaTemperatura.java
@@ -1,18 +1,29 @@
 import java.util.Observer;
 
 public class GraficaTemperatura implements Observer {
+    //especificar la dimension maxima del vector
 	private float _temp[];
+        private int ult_pos = 0;
+        final private int MAXTEMPS;
         
 	public Temperatura _sujetoObservable;
 
-	
+            //revisar
+    public GraficaTemperatura() {
+        this.MAXTEMPS = 7;
+    }
+
+	//descubir como va la grafica
         public void mostrarGrafica() {
 		throw new UnsupportedOperationException();
                
 	}
 
+        //creo que está bien pero repasar
     @Override
     public void update(java.util.Observable o, Object o1) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int pos_insertar = ult_pos % MAXTEMPS;
+        _temp[pos_insertar] = _sujetoObservable.getTemp();
+        ult_pos += 1;
     }
 }
