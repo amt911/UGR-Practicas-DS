4c9e5eb2d1b86d07526801ecde9f0aa8b9d023d9
Javier Serrano Lucas <javier.serluc@gmail.com>
2022-03-07 00:16:01 +0100

    Primera implementación de la grafica
    No se ha probado todavia

--- S1M1/src/GraficaTemperatura.java
+++ S1M1/src/GraficaTemperatura.java
@@ -8,7 +8,7 @@ import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;  
 import org.jfree.data.category.DefaultCategoryDataset;  
 
-public class GraficaTemperatura implements Observer {
+public class GraficaTemperatura extends JFrame implements Observer {
     //especificar la dimension maxima del vector
 	private float _temp[];
         private int ult_pos = 0;
@@ -17,23 +17,57 @@ public class GraficaTemperatura implements Observer {
 
     //revisar
     public GraficaTemperatura(Temperatura t) {
+        //constructor de la grafica
+        super("Grafica temperaturas");
+        
         this.MAXTEMPS = 7;
         _temp = new float[MAXTEMPS];
 	_sujetoObservable = t;
-
+    }
+    
+    //funcion para crear el dataset de la grafica
+    private DefaultCategoryDataset crearDataset(){
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+        String series1= "temperaturas semanales";
+        
+        dataset.addValue(_temp[0], series1, "lunes");  
+        dataset.addValue(_temp[1], series1, "martes");  
+        dataset.addValue(_temp[2], series1, "miercoles");  
+        dataset.addValue(_temp[3], series1, "jueves");  
+        dataset.addValue(_temp[4], series1, "viernes");  
+        dataset.addValue(_temp[5], series1, "sabado");  
+        dataset.addValue(_temp[6], series1, "domingo"); 
+        
+        return dataset;
     }
 
-	//descubir como va la grafica
+	
     public void mostrarGrafica() {
-        System.out.println("GRÁFICA Temp Celsius: " + _temp[ult_pos]);                 
-	}
+        //inicialización contexto de la gráfica
+        if(ult_pos >= 6 ){
+            this.setVisible(false);
+            DefaultCategoryDataset dataset = crearDataset();
+            //crea una grafica de puntos
+            JFreeChart chart = ChartFactory.createLineChart(  "Temperaturas", "dias semana",   "Temp. en Celsius",    dataset );
+            //crea un panel que incluye la grafica
+            ChartPanel panel = new ChartPanel(chart);
+            //Coloca el panel?
+            setContentPane(panel);
+            
+            this.setAlwaysOnTop(true);  
+            this.pack();                 //redimensiona la pagina dinamicamente
+            this.setSize(600, 400);      // tamaño de la ventana base
+            this.setVisible(true);          //vuelve a poner la ventana visile tras actualziar
+        }
+                         
+    }
 
     //creo que está bien pero repasar
     @Override
     public void update(java.util.Observable o, Object o1) {
-        int pos_insertar = ult_pos;
+        int pos_insertar = ult_pos % MAXTEMPS;
         _temp[pos_insertar] = _sujetoObservable.getTemp();
          mostrarGrafica();
-         ult_pos = (ult_pos+1) % MAXTEMPS;
+         ult_pos += 1;
     }
 }
