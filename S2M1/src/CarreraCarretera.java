062adf99f02db4b05bcfd98ec825bde9e5f8ca3d
Richi76 <93768820+Richi76@users.noreply.github.com>
2022-03-13 12:23:01 +0100

    He añadido StartRace, EndRace, un identificador a la carrera, y he hecho los constructores de cada uno

--- S2M1/src/CarreraCarretera.java
+++ S2M1/src/CarreraCarretera.java
@@ -1,8 +1,17 @@
-public class CarreraCarretera extends Carrera {
+public class CarreraCarretera extends Carrera implements Runnable {
 
-    @Override
-    public void run() {
-        // TODO Auto-generated method stub
-        
+    CarreraCarretera(int numberRace, ArrayList<_Bicicleta> bicicletas){
+        super(numberRace, bicicletas);
+    }
+
+    public void startRace(){
+        Ended = false;
+        System.out.println("La carrera de carretera  " + numberRace +" ha comenzado");
+
+    }
+
+    public void endRace(){
+        Ended = true;
+        System.out.println("La carrera de carretera  " + numberRace +" ha terminaddo");
     }
 }
