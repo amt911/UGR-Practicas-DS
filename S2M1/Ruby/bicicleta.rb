5beaf76a10377b21c44d37bda4e246fc6360ad73
Andrés Merlo Trujillo <a.merlo.truji10@gmail.com>
2022-03-13 19:44:38 +0100

    Implementacion Ruby

---
+++ S2M1/Ruby/bicicleta.rb
@@ -0,0 +1,11 @@
+class Bicicleta
+    def initialize(dorsal, tengoAcabar)
+        @dorsal = dorsal
+        @acabado = tengoAcabar
+    end
+
+    def acabar_carrera
+        @acabado = true
+    end
+
+end
